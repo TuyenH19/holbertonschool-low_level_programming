4ab576a84ae27640f2fdb6b1324d7f71d56be2a8
TuyenH19 <thtuyen199gmail.com>
2023-10-11 23:29:46 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,7 @@ int main()
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
-	printif("Size of a long int: %lu byte(s)\n", sizeof(longintType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(2longintType));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 	return (0);

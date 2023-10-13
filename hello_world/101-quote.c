a05ebb2991e4e945365f1e1a6e38cb2d40b5caa6
TuyenH19 <thtuyen199gmail.com>
2023-10-13 04:29:28 -0700

    commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
-int  main()
+int  main(void)
 {
-	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	char *s="and\ that\ piece\ of\ art\ is\ useful\"\ -\ Dora\ Korpar,\ 2015-10-19\n"; 
 	while (*s) putchar(*s++);
-
 	return (1);
 }

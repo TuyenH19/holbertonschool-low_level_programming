8253637551775569f93f70109ccc7be73d86cdb5
TuyenH19 <thtuyen199gmail.com>
2023-10-13 04:59:41 -0700

    commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int  main(void)
 {
-int len= str("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+int len=strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 
 write(1,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",len); 
 return (1);

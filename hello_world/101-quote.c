76f5c267b6619ad6c6d43f76b91d24d004473845
TuyenH19 <thtuyen199gmail.com>
2023-10-13 04:42:25 -0700

    commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int  main()
 {
-	write(1,"and tha piece of art is useful\" Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"); 
+	fwrite(1,"and tha piece of art is useful\" Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"); 
 	return (1);
 }

a282dd9afc802c481c4b8c21b07c45a2f6584b9d
TuyenH19 <thtuyen199gmail.com>
2023-10-13 05:25:35 -0700

    commit

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int  main(void)
 {
-char t[100]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-scanf("%c", t[100]); 
+char str[100]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+scanf("%s\n",str); 
 return (1);
 }

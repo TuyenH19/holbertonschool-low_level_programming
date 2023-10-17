c227a935ad1201678b6d2d238e81009818208bfe
TuyenH19 <thtuyen199gmail.com>
2023-10-17 13:24:41 -0700

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Create function print_alphabet_x10
+ *
+ * Return: Always 0 (Success)
+ */
+
+void  print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');	
+		i++;	
+	}
+}

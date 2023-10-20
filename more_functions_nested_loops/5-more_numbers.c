6413414c180d94a0b78adfbbb5cf8bff25ca144d
TuyenH19 <thtuyen199@gmail.com>
2023-10-20 06:43:44 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print 10 times from 0 to 14
+ *
+ * Return: 10 times frokm 0 to 14 and followed by new line
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 1; a <=10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar(b / 10 + '0');
+				_putchar(b % 10 + '0');
+			}
+			else
+				_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}

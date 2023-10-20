b68a470ce53bd5caa55f32cbf584faf211b6236e
TuyenH19 <thtuyen199@gmail.com>
2023-10-20 05:36:58 -0700

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - to check for uppercase character
+ *
+ * @ c: the value input
+ * Return: 1 if uppercase and 0 if not
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+	return (1);
+else
+	return (0);
+}

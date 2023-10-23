5ef6b2ec29a74e91023947a1e8d7df11d301da5b
TuyenH19 <thtuyen199@gmail.com>
2023-10-23 05:34:36 -0700

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ * @a: integer1
+ * @b: integer2
+ *
+ * Return: nothing.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	 c = *b;
+	 *b = *a;
+	 *a = c;
+	
+}	

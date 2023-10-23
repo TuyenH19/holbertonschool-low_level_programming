91a5089ca3c4b4dde77ef185d8821e8278f2890e
TuyenH19 <thtuyen199@gmail.com>
2023-10-23 06:51:37 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen -length of a string
+ * @s: string
+ *
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+	char *ptr;
+	
+	ptr = s;
+	len = 0;
+	while (*ptr != 0)
+	{
+		len++;
+		ptr++;
+	}
+	return (len);
+}

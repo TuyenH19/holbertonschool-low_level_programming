a322762d2968eef470ba8f3f4736624cd91d35a5
TuyenH19 <thtuyen199@gmail.com>
2023-10-24 07:55:00 -0700

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,7 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int index;
 
-	for (index = 0; src[index] >= '\0'; index++)
+	for (index = 0; src[index] != '\0'; index++)
 		dest[index] = src[index];
+	if (src[index] == '\0')
+		dest[index] = '\0';
 	return dest;
 }

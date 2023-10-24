a8eb87036a32e57f708acc2f57dea4e34e673361
TuyenH19 <thtuyen199@gmail.com>
2023-10-24 06:10:37 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of the string
+ * @str: string input
+ *
+ * Return: nothing.
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n;
+
+	while (str[len] != '\0')
+	{	
+		len++;
+	}
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+	while (n < len)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}

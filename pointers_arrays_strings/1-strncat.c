1e9b95f3b35825531740fb3bfdd57240e87bd7a5
TuyenH19 <thtuyen199@gmail.com>
2023-10-25 01:53:43 -0700

    1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strnchat - concatenate two strings with n characters
+ * @dest: string destination
+ * @src: string source
+ *
+ * Return: string destination
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index;
+	int len_dest = 0;
+
+	while (dest[len_dest] != '\0')
+		len_dest++;
+	for (index = 0; index < n; index++)
+		dest[len_dest + index] = src[index];
+	return (dest);
+}

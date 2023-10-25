f9dc67595e295fd472a7c70a3ddfa8b22457fb75
TuyenH19 <thtuyen199@gmail.com>
2023-10-25 06:10:45 -0700

    4-betty

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@
 void reverse_array(int *a, int n)
 {
 	int start = 0;
-	int end = n-1;
+	int end = n - 1;
 	int tmp;
 
 	while (start < end)
@@ -19,7 +19,7 @@ void reverse_array(int *a, int n)
 		tmp = a[start];
 		a[start] = a[end];
 		a[end] = tmp;
-		
+
 		start++;
 		end--;
 	}

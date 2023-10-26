02d3367e5f4719b43b3fed737779dd18c9701eb9
TuyenH19 <thtuyen199@gmail.com>
2023-10-26 02:41:29 -0700

    0.3

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * memset - fill memory with a constant byte
+ * @s: string input
+ * @b: constant
+ * @n: first n byte of memory
+ *
+ * Return: string that has been filled with a constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}

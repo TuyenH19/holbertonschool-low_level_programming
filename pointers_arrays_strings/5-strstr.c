57e3c4a9fc30eff5643e5231a2082e4c351b115c
TuyenH19 <thtuyen199@gmail.com>
2023-10-27 02:44:23 -0700

    5.c3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - search for the first occurence of a string inside anothers tring
+ * @haystack: specify the string to search
+ * @needle: specify the string to search for
+ *
+ * Return: a pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, len;
+
+	for (len = 0; haystack[len] != '\0'; len++)
+		len++;
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0' && j <= len + 1; j++)
+		{
+			if (haystack[j] == needle[i])
+				return (&haystack[j]);
+		}
+	}
+	return ('\0');
+}

ed6016d0a6e373d769e230f47f0d5c1836bbbc33
TuyenH19 <thtuyen199@gmail.com>
2023-10-27 01:21:47 -0700

    3.c3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,22 +11,24 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int count = 0;
+	unsigned int i, j;
+	unsigned int count, check;
 
+	count = 0;
 	for (i = 0; s[i] != '0'; i++)
 	{
+		check = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] != accept[j])
-				j++;
-			else
-				if (s[i] == accept[j])
-				{
-					count += 1;
-					break;
-				}
+			if (accept[j] == s[i])
+			{
+				count += 1;
+				check = 1;
+				break;
+			}
 		}
+		if (check == 0)
+			break;
 	}
-	return (count - 1);
+	return (count);
 }

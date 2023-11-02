9aa06b81dca12d5c7bee3c5bfdd057eb9a9c83be
TuyenH19 <thtuyen199@gmail.com>
2023-11-02 14:58:44 -0700

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entrty point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else 
+				sum = sum + atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

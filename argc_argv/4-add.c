67ca3a847318bbcb477d4579d039c5225bf3a01e
TuyenH19 <thtuyen199@gmail.com>
2023-11-02 15:33:15 -0700

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -28,9 +28,8 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-			else 
-				sum = sum + atoi(argv[i]);
 		}
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);

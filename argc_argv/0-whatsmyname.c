3a913475b1809ece8b3a5ceec1b35d1e31bd9569
TuyenH19 <thtuyen199@gmail.com>
2023-11-02 05:50:34 -0700

    0-betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc;

c0a3bc24dcdb506da5e649269dbca1e3ddae618b
TuyenH19 <thtuyen199@gmail.com>
2023-11-09 15:44:46 +0000

    2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * @void: nothing
+ *
+ * Return: name of file that was complied from
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

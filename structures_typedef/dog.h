746f36f5c8b77a8fa2f421467061a799a1bccbe7
TuyenH19 <thtuyen199@gmail.com>
2023-11-13 10:41:34 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: to define a new type of combination of more type
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

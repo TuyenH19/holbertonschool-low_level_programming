8d265fba3aad030430ecd9c6af58aca5670bf034
TuyenH19 <thtuyen199@gmail.com>
2023-11-15 21:48:42 +0000

    3-main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,7 +12,11 @@
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+	char *op;
 	int (*operation)(int, int);
+
+	operation = get_op_func(argv[2]);
 
 	if (argc != 4)
 	{
@@ -20,19 +24,22 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if ((atoi(argv[2]) == '/' || atoi(argv[2]) == '%') && atoi(argv[3]) == 0)
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if ((*op == '/' && num2 == 0) ||
+		(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	operation = get_op_func(argv[2]);
 
 	if (!operation)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", operation(atoi(argv[1]), atoi(argv[3])));
+	printf("%d\n", operation(num1, num2));
 	return (0);
 }

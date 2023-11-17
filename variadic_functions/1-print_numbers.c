337bf907810f6e6a7ee2f0ff1b75df11e0a6310d
TuyenH19 <thtuyen199@gmail.com>
2023-11-17 09:25:44 +0000

    1-betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,24 +6,20 @@
 * print_numbers - function to print number with separator
 * @separator: string between numbers
 * @n: number of integers
-* ...: variable number of parameters
 * Returns: numbers with separators
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
-
-	if (!separator)
-		return;
+	unsigned int index;
 
 	va_start(ap, n);
 
-	for (i = 0; i < n; i++)
+	for (index = 0; index < n; index++)
 	{
 		printf("%d", va_arg(ap, int));
 
-		if (i < n - 1)
+		if (separator != NULL && index < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");

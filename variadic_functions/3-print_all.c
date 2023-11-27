1116ceb180c1ee745cd0178635f0bd386ad4f6af
TuyenH19 <thtuyen199@gmail.com>
2023-11-27 07:46:36 +0000

    3-fix error segmentation fault

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,67 +1,6 @@
-#include "variadic_functions.h"
+#include "variadic_functions1.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-/**
- * print_char - print the arguments with only characters
- * @arg: arguments provided
- *
- * Return: characters
-*/
-void print_char(va_list arg)
-{
-	char char_arg = va_arg(arg, int);
-
-	printf("%c", char_arg);
-}
-
-/**
- * print_int - print the arguments with integer value
- * @arg: arguments provided
- *
- * Return: integers
-*/
-void print_int(va_list arg)
-{
-	int int_arg = va_arg(arg, int);
-
-	printf("%d", int_arg);
-}
-
-/**
- * print_float - print the arguments with float value
- * @arg: arguments provided
- *
- * Return: floats
-*/
-void print_float(va_list arg)
-{
-	float float_arg = va_arg(arg, double);
-
-	printf("%f", float_arg);
-}
-
-/**
- * print_string - print the arguments with string value
- * @arg: arguments provided
- *
- * Return: strings
-*/
-void print_string(va_list arg)
-{
-	char *str_arg;
-
-	str_arg = va_arg(arg, char *);
-
-	if (!str_arg)
-		str_arg = "(nil)";
-	printf("%s", str_arg);
-}
 
 /**
  * print_all - to print anything with format provided followed by a new line
@@ -69,33 +8,42 @@ void print_string(va_list arg)
  *
  * Return: a string with n parameters and separators
 */
-
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0, j = 0;
-	char *separator = "";
-	format_t func[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
+	va_list arg;
+	int i = 0;
+	char *str, *separator = "";
 
-	va_start(args, format);
-
-	while (format && (*(format + i)))
+	va_start(arg, format);
+	if (format)
 	{
-		while (j < 4 && (*(format + i) != *(func[j].symbol)))
-			j++;
-		if (j < 4)
+		while (format[i])
 		{
-			printf("%s", separator);
-			func[j].print(args);
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(arg, int));
+					break;
+				case 'i':
+					printf("%s%i", separator, va_arg(arg, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(arg, double));
+					break;
+				case 's':
+					str = va_arg(arg, char *);
+					if (!str)
+						str = "(nill)";
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
 			separator = ", ";
+			i++;
 		}
-		i++;
+		printf("\n");
+		va_end(arg);
 	}
-	printf("\n");
-	va_end(args);
 }

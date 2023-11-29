0f7214f73dbef1750387c7e073ef29272e1d8d2f
TuyenH19 <thtuyen199@gmail.com>
2023-11-29 11:17:00 +0000

    Task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all data of a dlistint_t
+ * @head: first node
+ * Return: sum of all data
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}

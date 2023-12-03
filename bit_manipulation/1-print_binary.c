55d68263ce161999418923d1f331b85bbe554a0d
TuyenH19 <thtuyen199@gmail.com>
2023-12-03 18:02:01 +0000

    Task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+void print_b(unsigned long int n);
+
+/**
+* print_binary - print binary representation of a number
+* @n: decimal number
+* Return: Nothing
+*/
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		print_b(n);
+	}
+}
+
+/**
+* print_b - print out binary
+* @n: decimal number
+* Return: Nothing
+*/
+void print_b(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	print_b(n >> 1);
+	if ((n & 1) == 1)
+		_putchar('1');
+	if ((n & 1) == 0)
+		_putchar('0');
+}

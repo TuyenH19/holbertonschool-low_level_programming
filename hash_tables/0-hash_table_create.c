a66293192b17daaa6f40536a4246420c4832ba65
TuyenH19 <thtuyen199@gmail.com>
2023-12-07 16:49:47 +0000

    Task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * 
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int index;
+	hash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+	
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	
+	for (index = 0; index < size; index++)
+		ht->array[index] = NULL;
+	
+	return (ht);
+}

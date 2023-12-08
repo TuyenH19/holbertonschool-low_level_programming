e617105794b6b1c93d2ae581a73667be89274ba4
TuyenH19 <thtuyen199@gmail.com>
2023-12-08 11:00:41 +0000

    Unfree the NULL newnode & fix betty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -36,7 +36,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (newnode == NULL)
 	{
 		free(valuedup);
-		free(newnode);
 		return (0);
 	}
 	newnode->key = strdup(key);

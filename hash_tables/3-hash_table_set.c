d2ab70cc7cb699edabb5736d5206ccf4a734bc81
TuyenH19 <thtuyen199@gmail.com>
2023-12-08 11:21:04 +0000

    edit condition of key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index_key, index;
 	hash_node_t *newnode;
 
-	if (ht == NULL || key == NULL || *key == '\n' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	valuedup = strdup(value);
